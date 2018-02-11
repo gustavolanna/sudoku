a0c73395b4c52817cfea5cc0bfa22c19a2fd32e2
yc26gl1 <gustavo.lanna@macys.com>
2018-02-11 17:28:50 -0200

    Refactoring + Unit Tests

--- src/main/java/com/zamro/sudoku/domain/SudokuGame.java
+++ src/main/java/com/zamro/sudoku/domain/SudokuGame.java
@@ -3,51 +3,79 @@ package com.zamro.sudoku.domain;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.*;
+
 public class SudokuGame {
 
 	public static final int BOARD_SIZE = 9;
 
-	public static final int BLOCK_SIZE = 3;
-
-	public static final int BOUND_VALUE = 9;
-
-	private static final int MAX_CLUES = 36;
-
-	private static final int MAX_CLUES_PER_BLOCK = 4;
+	private static final Integer[] NUMBERS = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
 
+	private final SudokuValidator validator = new SudokuValidator();
+
 	private Integer[][] board;
 
-	private SudokuValidator validator;
-
-	private static final Integer[] NUMBERS = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-
 	public SudokuGame(Integer[][] board) {
-		validator = new SudokuValidator(board);
+		validator.validate(board);
+		this.board = board;
 	}
 
 	public SudokuGame() {
 		board = new Integer[BOARD_SIZE][BOARD_SIZE];
-		createBoard(board, 0, 0);
-//		Random random = new Random();
+		fillBoard(board, 0, 0, randomNumbers());
 	}
 
-//	private List<List<Integer>> randomList(Random random) {
-//		List<List<Integer>> list = new LinkedList();
-//		for (int i = 0; i < BOARD_SIZE; i++) {
-//			List<Integer> blockNumbers = new LinkedList<>();
-//			List<Integer> numbers = new LinkedList<>(Arrays.asList(NUMBERS));
-//			while (blockNumbers.size() < BOARD_SIZE) {
-//				int index = random.nextInt(numbers.size());
-//				Integer element = numbers.remove(index);
-//				blockNumbers.add(element);
-//			}
-//			list.add(blockNumbers);
-//		}
-//		return list;
-//	}
+	public Integer[][] getBoard() {
+		return board;
+	}
 
+	public boolean validate() {
+		return validator.validate(this);
+	}
+
+	public List<Integer> getInvalidPositions() {
+		return validator.validateGame(board);
+	}
+
+	private List<Integer> randomNumbers() {
+		Random random = new Random();
+		List<Integer> orderedList = new LinkedList(Arrays.asList(NUMBERS));
+		List<Integer> randomList = new ArrayList<>();
+		while (!orderedList.isEmpty()) {
+			Integer randomValue = orderedList.remove(random.nextInt(orderedList.size()));
+			randomList.add(randomValue);
+		}
+		return randomList;
+	}
+
+	public boolean fillBoard() {
+		return fillBoard(board, 0, 0, randomNumbers());
+	}
+
+	private boolean fillBoard(Integer[][] game, int x, int y, List<Integer> numbers) {
+		if (x >= game[y].length) {
+			x = 0;
+			if (++y >= game.length) {
+				return true;
+			}
+		}
+		if (game[y][x] != null) {
+			return fillBoard(game, x + 1, y, numbers);
+		} else {
+			for (Integer number : numbers) {
+				if (validator.isValidAssignment(game, x, y, number)) {
+					game[y][x] = number;
+					if (fillBoard(game, x + 1, y, numbers)) {
+						return true;
+					}
+					game[y][x] = null;
+				}
+			}
+		}
+		return false;
+	}
 
 	@Override
 	public String toString() {
@@ -70,65 +98,4 @@ public class SudokuGame {
 		return sb.toString();
 	}
 
-
-	public boolean createBoard(Integer[][] game, int x, int y) {
-		if (x >= game[y].length) {
-			x = 0;
-			if (++y >= game.length) {
-				return true;
-			}
-		}
-		for (int num = 1; num <= 9; num++) {
-			if (isValidAssignment(game, x, y, num)) {
-				game[y][x] = num;
-				if (createBoard(game, x + 1, y)) {
-					return true;
-				}
-				game[y][x] = null;
-			}
-		}
-		return false;
-	}
-
-	private boolean isValidAssignment(Integer[][] game, int x, int y, int value) {
-		return !hasValueInRow(game[y], value) &&
-				!hasValueInCol(game, x, value) &&
-				!hasValueInBlock(game, x, y, value);
-	}
-
-	private boolean hasValueInBlock(Integer[][] game, int x, int y, int value) {
-		int offSetX = x - (x % BLOCK_SIZE) + BLOCK_SIZE;
-		int offSetY = y - (y % BLOCK_SIZE) + BLOCK_SIZE;
-		for (int startY = y - (y % BLOCK_SIZE); startY < offSetY; startY++) {
-			for (int startX = x - (x % BLOCK_SIZE); startX < offSetX; startX++) {
-				if (game[startY][startX] != null && game[startY][startX] == value) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
-
-	private boolean hasValueInCol(Integer[][] game, int x, int value) {
-		for (int y = 0; y < game.length; y++) {
-			if (game[y][x] != null && game[y][x] == value) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	private boolean hasValueInRow(Integer[] row, int value) {
-		for (int x = 0; x < row.length; x++) {
-			if (row[x] != null && row[x] == value) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	public static void main(String[] args) {
-		SudokuGame sudokuGame = new SudokuGame();
-		System.out.println(sudokuGame.toString());
-	}
 }

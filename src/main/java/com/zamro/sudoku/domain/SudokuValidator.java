899c4011c3e8ce67d791b4ec52649d3d4acda83a
yc26gl1 <gustavo.lanna@macys.com>
2018-02-11 17:28:50 -0200

    Refactoring + Unit Tests

--- src/main/java/com/zamro/sudoku/domain/SudokuValidator.java
+++ src/main/java/com/zamro/sudoku/domain/SudokuValidator.java
@@ -1,18 +1,21 @@
 package com.zamro.sudoku.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SudokuValidator {
 
-	public SudokuValidator(Integer[][] board) {
-		validateBoard(board);
+	public static final int BLOCK_SIZE = 3;
+
+	public boolean validate(SudokuGame game) {
+		return validate(game.getBoard()) && validateGame(game.getBoard()).isEmpty();
 	}
 
-	public void validateBoard(Integer[][] board) {
-		validateBoundaries(board);
-		validateValues(board);
-
+	public boolean validate(Integer[][] board) {
+		return validateBoundaries(board) && validateValues(board);
 	}
 
-	private void validateBoundaries(Integer[][] board) {
+	private boolean validateBoundaries(Integer[][] board) {
 		if (board.length != SudokuGame.BOARD_SIZE) {
 			throw new IllegalArgumentException("Invalid board size. It should be a 9 x 9 matrix");
 		}
@@ -21,17 +24,76 @@ public class SudokuValidator {
 				throw new IllegalArgumentException("Invalid board size. It should be a 9 x 9 matrix");
 			}
 		}
+		return true;
 	}
 
-	private void validateValues(Integer[][] board) {
-		for (int x = 0; x < board.length; x++) {
-			for (int y = 0; y < board[x].length; y++) {
-				Integer value = board[x][y];
-				if (value != null && (value < 0 || value > 9)) {
-					throw new IllegalArgumentException("Invalid board value at [" + x + ", " + y + "]. Expected value 0-9, found " + value);
+	private boolean validateValues(Integer[][] board) {
+		for (int y = 0; y < board.length; y++) {
+			for (int x = 0; x < board[y].length; x++) {
+				Integer value = board[y][x];
+				if (value != null && (value <= 0 || value > 9)) {
+					throw new IllegalArgumentException("Invalid board value at [" + y + ", " + x + "]. Expected value 1-9 or null, found " + value);
 				}
 			}
 		}
+		return true;
+	}
+
+	public List<Integer> validateGame(Integer[][] board) {
+		validate(board);
+		List<Integer> invalidPositions = new ArrayList<>();
+		for (int y = 0; y < board.length; y++) {
+			for (int x = 0; x < board[y].length; x++) {
+				Integer currentValue = board[y][x];
+				board[y][x] = null;
+				if (!isValidAssignment(board, x, y, currentValue)) {
+					invalidPositions.add(matrixToArray(x, y));
+				}
+				board[y][x] = currentValue;
+			}
+		}
+		return invalidPositions;
+	}
+
+	private int matrixToArray(int x, int y) {
+		return y * SudokuGame.BOARD_SIZE + x;
+	}
+
+	public boolean isValidAssignment(Integer[][] game, int x, int y, Integer number) {
+		return !hasValueInRow(game[y], number) &&
+				!hasValueInCol(game, x, number) &&
+				!hasValueInBlock(game, x, y, number);
+	}
+
+	private boolean hasValueInBlock(Integer[][] game, int x, int y, int value) {
+		int offSetX = x - (x % BLOCK_SIZE) + BLOCK_SIZE;
+		int offSetY = y - (y % BLOCK_SIZE) + BLOCK_SIZE;
+		for (int startY = y - (y % BLOCK_SIZE); startY < offSetY; startY++) {
+			for (int startX = x - (x % BLOCK_SIZE); startX < offSetX; startX++) {
+				if (game[startY][startX] != null && game[startY][startX] == value) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	private boolean hasValueInCol(Integer[][] game, int x, int value) {
+		for (int y = 0; y < game.length; y++) {
+			if (game[y][x] != null && game[y][x] == value) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private boolean hasValueInRow(Integer[] row, int value) {
+		for (int x = 0; x < row.length; x++) {
+			if (row[x] != null && row[x] == value) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
---
+++ src/test/java/com/zamro/sudoku/domain/SudokuGameTest.java
@@ -0,0 +1,156 @@
+package com.zamro.sudoku.domain;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class SudokuGameTest {
+
+	@Test
+	public void shouldCreateValidBoardWithNoArgumentConstructor() {
+		assertTrue(new SudokuGame().validate());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void shouldThrowIllegalArgumentExceptionForBoardsWithInvalidDimension() {
+		Integer[][] board = {
+				{2, 7, 1,  9, 5, 6,  3, 4, 4, 8},
+				{9, 5, 6,  3, 4, 8,  2, 7, 1},
+				{3, 4, 8,  2, 7, 1,  9, 5, 6},
+
+				{7, 2, 9,  1, 6, 5,  4, 8, 3},
+				{1, 6, 5,  4, 8, 3,  7, 2, 9},
+				{4, 8, 3,  7, 2, 9,  1, 6, 5},
+
+				{5, 1, 2,  6, 9, 7,  8, 3, 4},
+				{6, 9, 7,  8, 3, 4,  5, 1, 2},
+				{8, 3, 4,  5, 1, 2,  6, 9, 7}
+		};
+		new SudokuGame(board);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void shouldThrowIllegalArgumentExceptionForBoardsWithInvalidValues() {
+		Integer[][] board = {
+				{2, 7, 1,  9, 5, 6,  3, 4, 0},
+				{9, 5, 6,  3, 4, 8,  2, 7, 1},
+				{3, 4, 8,  2, 7, 1,  9, 5, 6},
+
+				{7, 2, 9,  1, 6, 5,  4, 8, 3},
+				{1, 6, 5,  4, 8, 3,  7, 2, 9},
+				{4, 8, 3,  7, 2, 9,  1, 6, 5},
+
+				{5, 1, 2,  6, 9, 7,  8, 3, 4},
+				{6, 9, 7,  8, 3, 4,  5, 1, 2},
+				{8, 3, 4,  5, 1, 2,  6, 9, 7}
+		};
+		new SudokuGame(board);
+	}
+
+	@Test
+	public void shouldNotValidateBoardForDuplicatedNumbersInRow() {
+		Integer[][] board = {
+				{2, 2, 1,  9, 5, 6,  3, 4, 8},
+				{9, 5, 6,  3, 4, 8,  2, 7, 1},
+				{3, 4, 8,  2, 7, 1,  9, 5, 6},
+
+				{7, 2, 9,  1, 6, 5,  4, 8, 3},
+				{1, 6, 5,  4, 8, 3,  7, 2, 9},
+				{4, 8, 3,  7, 2, 9,  1, 6, 5},
+
+				{5, 1, 2,  6, 9, 7,  8, 3, 4},
+				{6, 9, 7,  8, 3, 4,  5, 1, 2},
+				{8, 3, 4,  5, 1, 2,  6, 9, 7}
+		};
+		SudokuGame game = new SudokuGame(board);
+		assertFalse(game.validate());
+	}
+
+	@Test
+	public void shouldNotValidateBoardForDuplicatedNumbersInCol() {
+		Integer[][] board = {
+				{2, 7, 1,  9, 5, 6,  3, 4, 8},
+				{2, 5, 6,  3, 4, 8,  2, 7, 1},
+				{3, 4, 8,  2, 7, 1,  9, 5, 6},
+
+				{7, 2, 9,  1, 6, 5,  4, 8, 3},
+				{1, 6, 5,  4, 8, 3,  7, 2, 9},
+				{4, 8, 3,  7, 2, 9,  1, 6, 5},
+
+				{5, 1, 2,  6, 9, 7,  8, 3, 4},
+				{6, 9, 7,  8, 3, 4,  5, 1, 2},
+				{8, 3, 4,  5, 1, 2,  6, 9, 7}
+		};
+		SudokuGame game = new SudokuGame(board);
+		assertFalse(game.validate());
+	}
+
+	@Test
+	public void shouldNotValidateBoardForDuplicatedNumbersInBlock() {
+		Integer[][] board = {
+				{2, 7, 1,  9, 5, 6,  3, 4, 8},
+				{9, 5, 6,  3, 4, 8,  2, 7, 1},
+				{3, 4, 2,  2, 7, 1,  9, 5, 6},
+
+				{7, 2, 9,  1, 6, 5,  4, 8, 3},
+				{1, 6, 5,  4, 8, 3,  7, 2, 9},
+				{4, 8, 3,  7, 2, 9,  1, 6, 5},
+
+				{5, 1, 2,  6, 9, 7,  8, 3, 4},
+				{6, 9, 7,  8, 3, 4,  5, 1, 2},
+				{8, 3, 4,  5, 1, 2,  6, 9, 7}
+		};
+		SudokuGame game = new SudokuGame(board);
+		assertFalse(game.validate());
+	}
+
+	@Test
+	public void shouldSolveBoardForIncompleteGame() {
+		Integer n = null;
+		Integer[][] board = {
+				{2, n, 1,  n, n, 6,  n, n, n},
+				{9, 5, n,  3, n, 8,  2, n, 1},
+				{n, n, n,  2, n, n,  9, 5, n},
+
+				{7, n, 9,  1, 6, n,  4, n, n},
+				{1, n, n,  4, n, n,  n, n, 9},
+				{4, n, n,  n, n, n,  n, n, 5},
+
+				{5, n, n,  6, n, 7,  n, n, 4},
+				{n, n, 7,  n, n, n,  n, n, n},
+				{8, n, 4,  5, n, n,  6, n, 7}
+		};
+		SudokuGame game = new SudokuGame(board);
+		game.fillBoard();
+		assertTrue(game.validate());
+	}
+
+	@Test
+	public void shouldReturnInvalidPositionsForInvalidGame() {
+		Integer[][] board = {
+				{2, 7, 1,  9, 5, 6,  3, 4, 8},
+				{9, 5, 6,  3, 4, 8,  2, 7, 1},
+				{3, 4, 2,  2, 7, 1,  9, 5, 6},
+
+				{7, 2, 9,  1, 6, 5,  4, 8, 3},
+				{1, 6, 5,  4, 8, 3,  7, 2, 9},
+				{4, 8, 3,  7, 2, 9,  1, 6, 5},
+
+				{5, 1, 2,  6, 9, 7,  8, 3, 4},
+				{6, 9, 7,  8, 3, 4,  5, 1, 2},
+				{8, 3, 4,  5, 1, 2,  6, 9, 7}
+		};
+		SudokuGame game = new SudokuGame(board);
+		List<Integer> invalidPositions = game.getInvalidPositions();
+		assertEquals(invalidPositions.size(), 4);
+		assertTrue(invalidPositions.get(0) == 0);
+		assertTrue(invalidPositions.get(1) == 20);
+		assertTrue(invalidPositions.get(2) == 21);
+		assertTrue(invalidPositions.get(3) == 56);
+	}
+
+}
